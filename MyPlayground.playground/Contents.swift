6e225b9909cf8a872a2314da075e72bf6b5ddde4
shannonbit <shannonbit@icloud.com>
2017-11-26 07:38:47 -0600

    El playground, variables y constantes

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var age: Int = 31
+age
+age += 1
+age
+var saludation = "Hola"
+var name = "shannonbit"
+saludation = "Hola \(name), tu tienes \(age) años"
