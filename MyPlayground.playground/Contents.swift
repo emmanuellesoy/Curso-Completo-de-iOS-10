77218afcee6770e5269cb6b496a193d55e72b17b
shannonbit <shannonbit@icloud.com>
2017-11-26 07:52:56 -0600

    Se elimina el ++ y el --

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,11 +1,3 @@
-//: Playground - noun: a place where people can play
-
-import UIKit
-
 var age: Int = 31
-age
-age += 1
-age
-var saludation = "Hola"
-var name = "shannonbit"
-saludation = "Hola \(name), tu tienes \(age) años"
+age += 1 // ++ hass been deprecated
+age -= 1
