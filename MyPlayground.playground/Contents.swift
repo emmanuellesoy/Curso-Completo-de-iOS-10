4a445014332624e21bec9be13db2a657036a16e0
shannonbit <shannonbit@icloud.com>
2017-11-26 08:34:35 -0600

    NS classes

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,7 @@
-import UIKit
-let red = UIColor.red
-let green = #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1)
+import Foundation
+//Se quita NS de algunas clases
+//Antes let date : NSDate = NSDate()
+let date : Date = Date()
+let dateFormater : DateFormatter
+let timer : Timer
+
