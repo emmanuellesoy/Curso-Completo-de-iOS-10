4484d3ab79ff922bdaa9c6472fe6d1d3f4aecf84
shannonbit <shannonbit@icloud.com>
2017-11-26 08:23:58 -0600

    Arreglos

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,10 +1,3 @@
-enum Direction {
-    case north
-    case northEast
-    case south
-    case east
-    case west
-}
-
-Direction.west
-    
+var food = ["Pizza", "Macarrones", "Alitas"]
+food.append("Hot-Dog")
+food.insert("Hamburguesa", at: 2)
