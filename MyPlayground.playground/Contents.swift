a8c68e33d68aea92c17c1d4c9158c90a1b4a4256
shannonbit <shannonbit@icloud.com>
2017-11-26 08:25:54 -0600

    Colors

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,3 @@
-var food = ["Pizza", "Macarrones", "Alitas"]
-food.append("Hot-Dog")
-food.insert("Hamburguesa", at: 2)
+import UIKit
+let red = UIColor.red
+let green = #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1)
