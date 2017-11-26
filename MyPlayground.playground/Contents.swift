a6cbc5d92c443287812f497dfaef21bffe9f7aa8
shannonbit <shannonbit@icloud.com>
2017-11-26 08:07:49 -0600

    Funciones y llamados de funciones

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,12 +1,5 @@
-for i in 0..<10 {
-    print("Hola: \(i)")
+func addTwoNumbers(x: Int, y: Int) -> Int {
+    return x+y
 }
 
-for i in 0...9 { // 0 <= 9 equivalent
-    print("Adios: \(i)")
-}
-
-for _ in 0..<10 { //If you don't use initialitation var
-    print("Hi")
-}
-
+addTwoNumbers(x: 3, y: 2)
